42a741c9e1cc8757dc16cf53925a75fa3fc3b562
Rve27 <rve27github@gmail.com>
2024-09-08 21:04:50 +0700

    ci: Add function to create release
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- ci.sh
+++ ci.sh
@@ -3,13 +3,15 @@
 base=$(dirname "$(readlink -f "$0")")
 install=$base/install
 src=$base/src
+repo_owner="Rv-Trees"
+repo_name="RvClang"
 
 set -eu
 
 function parse_parameters() {
     while (($#)); do
         case $1 in
-            all | binutils | deps | kernel | llvm | clangversion) action=$1 ;;
+            all | binutils | deps | kernel | llvm | clangversion | createrelease) action=$1 ;;
             *) exit 33 ;;
         esac
         shift
@@ -21,6 +23,7 @@ function do_all() {
     do_llvm
     do_binutils
     do_clangversion
+    do_createrelease
     do_kernel
 }
 
@@ -113,6 +116,35 @@ function do_clangversion() {
     clang_version="$("$base"/install/bin/clang --version | head -n1 | cut -d' ' -f4)"
     file="RvClang-$clang_version.tar.gz"
 }
+
+function do_createrelease() {
+    TAG_NAME="RvClang-$clang_version-$(date +'%Y%m%d')"
+    RELEASE_NAME="$TAG_NAME"
+    RELEASE_BODY="$TAG_NAME"
+
+    if git rev-parse "$TAG_NAME" >/dev/null 2>&1; then
+        echo "Tag $TAG_NAME already exists"
+    else
+        git tag "$TAG_NAME"
+        git push origin "$TAG_NAME"
+    fi
+
+    RELEASE_RESPONSE=$(curl -s -X POST \
+        -H "Authorization: token $GITHUB_TOKEN" \
+        -d "{\"tag_name\":\"$TAG_NAME\", \"target_commitish\":\"main\", \"name\":\"$RELEASE_NAME\", \"body\":\"$RELEASE_BODY\", \"draft\":false, \"prerelease\":false}" \
+        "https://api.github.com/repos/$repo_owner/$repo_name/releases")
+
+    RELEASE_ID=$(echo "$RELEASE_RESPONSE" | jq -r '.id')
+
+    if [ "$RELEASE_ID" == "null" ] || [ -z "$RELEASE_ID" ]; then
+        echo "Failed to create release"
+        echo "Response: $RELEASE_RESPONSE"
+        exit 1
+    else
+        echo "Release created successfully with ID: $RELEASE_ID"
+    fi
+}
+
 
 parse_parameters "$@"
 do_"${action:=all}"
